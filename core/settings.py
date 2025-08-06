13576bcbb6cd19585811a86ccd678e6fbdee0d74
Godlives08 <manuel.astacio@outlook.com>
2025-08-06 15:38:55 -0400

    Reset database user credentials in settings.py for security purposes

--- core/settings.py
+++ core/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'clinica_facil',
-        'USER': 'UserSystem',
-        'PASSWORD': 'Wqh5$Rsu67e.',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
